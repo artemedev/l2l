9fd507877341dae7e6615430358771b4e80aa719
aeegorov <aeegorov@medtechtd.ru>
2025-07-18 14:33:25 +0300

    Удалил не нужные таблицы из локальной базы данных

--- l2l-aggregator/Services/Database/DatabaseService.cs
+++ l2l-aggregator/Services/Database/DatabaseService.cs
@@ -4,18 +4,15 @@ namespace l2l_aggregator.Services.Database
 {
     public class DatabaseService
     {
-        public IUserAuthRepository UserAuth { get; }
         public IConfigRepository Config { get; }
         public INotificationLogRepository NotificationLog { get; }
 
         public IAggregationStateRepository AggregationState { get; }
         public DatabaseService(
-            IUserAuthRepository userAuth,
             IConfigRepository config,
             INotificationLogRepository notificationLog,
             IAggregationStateRepository aggregationState)
         {
-            UserAuth = userAuth;
             Config = config;
             NotificationLog = notificationLog;
             AggregationState = aggregationState;
